e13e8efdb9368acf05801435cf5a1f6b95978076
William Gasparetto <gasparetto.w@gmail.com>
2022-07-05 11:50:03 -0300

    feat: add type file

---
+++ src/common/types.ts
@@ -0,0 +1,60 @@
+import { MutableRefObject } from 'react'
+
+export interface Dragon {
+  name: string
+  type: string
+  id?: string
+  createdAt?: string
+  updatedAt?: string
+  histories?: History[]
+}
+
+export interface DragonTypes {
+  fogo: string
+  agua: string
+  areia: string
+  noturno: string
+  venenoso: string
+}
+
+export type InputProps = {
+  type?: string
+  name?: string
+  id?: string
+  placeholder?: string
+  value?: string
+  prefixIcon?: string
+  sufixIcon?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  ref?: React.Ref<HTMLInputElement> | MutableRefObject<HTMLInputElement>
+  size?: 'sm' | 'md' | 'lg'
+  required?: boolean
+}
+
+export type CardProps = {
+  children?: React.ReactNode
+  title?: string
+  description?: string
+  image?: string
+}
+
+export type DragonCardProps = CardProps & {
+  id?: string
+  to: string
+  tag: string
+}
+
+export type User = {
+  username: string
+  password: string
+}
+
+export type Errors = {
+  username?: string
+  password?: string
+}
+
+export type SortSwitcher = {
+  sort: string
+  order?: 'asc' | 'desc'
+}
